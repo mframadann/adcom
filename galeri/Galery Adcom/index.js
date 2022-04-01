61f1095119513250055fa46a8cf9ed9b10b38657
Ramadandev <dan.alphadev@gmail.com>
2022-04-01 22:43:19 +0700

    demo-web-first

---
+++ galeri/Galery Adcom/index.js
@@ -0,0 +1,74 @@
+gsap.registerPlugin(TextPlugin);
+gsap.to(".judul", { duration: 3, text: "Galeri Kegiatan Adcom " })
+gsap.from('#id1', {
+    duration: 1,
+    delay: 2,
+    rotationX: 360,
+    opacity: 0
+});
+gsap.from('#id2', {
+    duration: 1,
+    delay: 3,
+    rotationY: 360,
+    opacity: 0
+});
+gsap.from('#id3', {
+    duration: 1,
+    delay: 4,
+    rotationX: 360,
+    opacity: 0
+});
+gsap.from('#id4', {
+    duration: 1,
+    delay: 5,
+    rotationY: 360,
+    opacity: 0
+});
+gsap.from('#id5', {
+    duration: 1,
+    delay: 6,
+    rotationX: 360,
+    opacity: 0
+});
+gsap.from('#id6', {
+    duration: 1,
+    delay: 7,
+    rotationY: 360,
+    opacity: 0
+});
+gsap.from('#id7', {
+    duration: 1,
+    delay: 8,
+    rotationX: 360,
+    opacity: 0
+});
+gsap.from('#id8', {
+    duration: 1,
+    delay: 9,
+    rotationY: 360,
+    opacity: 0
+});
+gsap.from('#id9', {
+    duration: 1,
+    delay: 10,
+    rotationX: 360,
+    opacity: 0
+});
+gsap.from('#id10', {
+    duration: 1,
+    delay: 11,
+    rotationY: 360,
+    opacity: 0
+});
+gsap.from('#id11', {
+    duration: 1,
+    delay: 12,
+    rotationX: 360,
+    opacity: 0
+});
+gsap.from('#id12', {
+    duration: 1,
+    delay: 13,
+    rotationY: 360,
+    opacity: 0
+});
---
+++ profile-ukm/index.js
@@ -0,0 +1,3 @@
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
